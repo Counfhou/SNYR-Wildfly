ed3d7c9ffa976370f4ac183356869c36d5977ae3
Yoran <Yoran@192.168.1.14>
2016-08-25 21:40:14 +0200

    - Added ping

---
+++ src/main/java/resources/Ping.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resources;
+
+import entity.Student;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author sande
+ */
+@Path("ping")
+@Transactional
+@ApplicationScoped
+public class Ping 
+{
+    @GET
+    public void getPing ()
+    {
+        System.out.println("Ping arrived.");
+    }
+}
